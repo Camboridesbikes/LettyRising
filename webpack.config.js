8216ff71cda45f1bfad8d9f780360544a5d9ec5c
Cameron Hattendorf <hattendorfcameron@gmail.com>
2021-01-04 17:10:32 -0800

    added webpack seetings for css and image assets

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
                 resolve: {
                   fullySpecified: false
                 }
+            },
+            {
+                test: /\.(css)$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
     },
