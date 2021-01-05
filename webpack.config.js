71a61f41c50138a0f3d480987bf32bd024565537
Cameron Hattendorf <hattendorfcameron@gmail.com>
2021-01-04 22:53:09 -0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry:  './src/index.js',
     module: {
         rules: [
             {
